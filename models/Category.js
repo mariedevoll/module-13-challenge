05d6284f35f7e59101cbc383e2cec84df01135ee
mariedevoll <marie.devoll@gmail.com>
2024-05-02 20:11:04 -0400

    establishing my models

--- models/Category.js
+++ models/Category.js
@@ -4,11 +4,18 @@ const sequelize = require('../config/connection.js');
 
 class Category extends Model {}
 
-Category.init(
-  {
-    // define columns
+Category.init({
+  id: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true
   },
-  {
+  category_name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+  },{
     sequelize,
     timestamps: false,
     freezeTableName: true,
